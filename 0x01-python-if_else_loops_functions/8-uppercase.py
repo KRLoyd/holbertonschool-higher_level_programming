a78de0abaa2789406e5a5ccf361f86ababe30489
KRLoyd <loydkristen@gmail.com>
2017-05-02 20:34:52 +0000

    fixed code to use only 2 prints

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str[:]:
+    for i in str:
         # check if character is lowercase
         if ord(i) > 96 and ord(i) < 123:
-            print("{:s}".format(chr(ord(i) - 32)), end="")
-        else:  # if character is not lowercase
-            print("{:s}".format(i), end="")
+            i = chr(ord(i) - 32)
+        print("{:s}".format(i), end="")
     print()
