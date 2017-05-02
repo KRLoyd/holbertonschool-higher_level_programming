ed70e114dfcd823592edfe55f79e52b7da8c87c1
KRLoyd <loydkristen@gmail.com>
2017-05-02 19:16:16 +0000

    foix last_digit so it can be negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,15 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    abs_number = -number
+    last_digit = -number % 10
+    last_digit *= -1
 else:
-    abs_number = number
-last_digit = abs_number % 10
-if last_digit == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
-elif last_digit > 5:
+    last_digit = number % 10
+if last_digit > 5:
     print("Last digit of {:d} is {:d} and is greater than 5"
           .format(number, last_digit))
-else:  # last_digit < 6
+elif last_digit < 6:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0".
           format(number, last_digit))
+else:  # last_digit == 0:
+    print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
