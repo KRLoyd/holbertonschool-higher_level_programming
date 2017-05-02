b69e22a62b9a6b8b5dbf556626db4a67d43fbdf2
KRLoyd <loydkristen@gmail.com>
2017-05-02 19:57:38 +0000

    indent comment

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-# check if c is lowercase
+    # check if c is lowercase
     if ord(c) > 96 and ord(c) < 123:
         return True
     else:  # not a lowercase character
