7281b96fb436685d259ebddfd82fdec1e4164a15
KRLoyd <loydkristen@gmail.com>
2017-05-03 15:25:41 +0000

    add check for n == 0

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
+    if n == 0:
+        return str[n + 1:]
     if n > 0:
-        first_part = str[:n]
-        last_part = str[n + 1:]
         if n < len(str):
-            return first_part + last_part
-        else:
-            return first_part
-    else:
+            return str[:n] + str[n + 1:]
+        else:  # n >= len(str)
+            return str
+    else:  # n < 0
         return str
