7c58f672bb2a5a26f7929f7271114952feb50294
KRLoyd <loydkristen@gmail.com>
2017-05-04 20:35:37 +0000

    add addition functionality

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
-    num_args = len(argv) - 1
-    if num_args != 3:
+    num_args = len(argv)
+    if num_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
