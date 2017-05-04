50c3d1204a75d1eb3cdd88eb1b8c5a200b622593
KRLoyd <loydkristen@gmail.com>
2017-05-04 20:26:54 +0000

    Create 100-my_calculator.py.
    Program that imports all functions from file calculator-1.py and handles basic operations.
    - make check for arguments != 3

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    num_args = len(argv) - 1
+    if num_args != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
