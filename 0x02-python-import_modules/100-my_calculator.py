fc5c5da9d688f3f327908378244aedca5fef67e3
KRLoyd <loydkristen@gmail.com>
2017-05-04 20:51:46 +0000

    add exit status for unknown operator and fix indentation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,19 +2,23 @@
 if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
+
     num_args = len(argv)
     if num_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
     a = int(argv[1])
     b = int(argv[3])
+
     if argv[2] == '+':
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif argv[2] == '-':
-         print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     elif argv[2] == '*':
-         print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     elif argv[2] == '/':
-         print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:  # operator is not above
-         print("Unknown operator. Available operators: +, -, * and /")
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
