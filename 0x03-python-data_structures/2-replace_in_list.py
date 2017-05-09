1b50bdf2055eb7efac1b31834c436d8248b483a8
KRLoyd <loydkristen@gmail.com>
2017-05-09 19:01:53 +0000

    Create 2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """ My function to replace an element in a list at idx
+
+    Args: list, idx to replace, replacing element
+
+    Returns: modified list, or original list if idx is out of range
+    """
+    if idx > len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
