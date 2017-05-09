cf3c6b5bf1a31355fee7335f2ad82831db8d9a7a
KRLoyd <loydkristen@gmail.com>
2017-05-09 19:11:20 +0000

    Add check for idx < -(len(my_list))

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,7 +6,8 @@ def element_at(my_list, idx):
 
     Return: element at idx
     """
-    if idx > len(my_list):
+    print("len(my_list): {:d}".format(-len(my_list)))
+    if idx > len(my_list) or idx < -(len(my_list)):
         return None
     else:
         return my_list[idx]
