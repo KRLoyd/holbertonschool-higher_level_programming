92f6ec95ae0c009e370e40b15766c633a8c25b0a
KRLoyd <loydkristen@gmail.com>
2017-05-10 01:12:24 +0000

    remove unneeded check and correct if idx = 0

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -11,15 +11,11 @@ def delete_at(my_list=[], idx=0):
     # Check that list exists
     if my_list is None:
         return None
-    # Check if list is empty
-    if list_len is 0:
-        return my_list
     # Check that idx is not out of range
-    if idx <= list_len and idx > 0:
+    if idx <= list_len - 1 and idx >= 0:
         # Delete specified index
-        copy_list = my_list
-        del(copy_list[idx])
+        del my_list[idx]
     else:
         return my_list
     # Return modified list
-    return copy_list
+    return my_list
