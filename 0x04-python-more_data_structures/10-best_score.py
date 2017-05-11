a541a00e8d39a1807850f33b3924b2918d44b065
KRLoyd <loydkristen@gmail.com>
2017-05-11 22:43:50 +0000

    Removed seperate check for dictionary and sorted list instead of using for loop

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,16 +6,6 @@ def best_score(my_dict):
 
     Return: key with biggest integer value
     """
-
-    # Check if dictionary exists
-    if my_dict is None:
-        return None
-
-    largest = 0
-    # Loop through the dictionary and compare values
-    for key, value in my_dict.items():
-        if value > largest:
-            largest = value
-            largest_key = key
-
-    return largest_key
+    # Make sorted list
+    if my_dict:
+        return(sorted(my_dict)[-1])
