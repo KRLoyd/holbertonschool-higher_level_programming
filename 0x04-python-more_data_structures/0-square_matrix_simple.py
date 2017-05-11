194ec0ed1624c0c443cdf270c186ac71100d5f23
KRLoyd <loydkristen@gmail.com>
2017-05-11 19:35:21 +0000

    Create file, copy original matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """ My function that computes the square value of all integers of a matrix.
+
+    Arg: matrix to evaluate
+
+    Return: new matrix, each value is the square value of input
+    """
+    # Check if matrix exists
+    if matrix is None:
+        return None
+    # Check if matrix is empty
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        print()
+        return
+
+    # Copy original matrix to new matrix
+    new_matrix = matrix
