5e21082a087ea96f5dc9ce7de5ea9aa568820d4e
KRLoyd <loydkristen@gmail.com>
2017-05-11 22:57:52 +0000

    Remove description to satisfy line count requirement

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    """ Multiplies all values by a number without a loop
-
-    Args: list to be evaluated, number to multiply by
-
-    Return: list of new values
-    """
-    new_list = list(map(lambda x: x * number, my_list))
-    return new_list
+    return(list(map(lambda x: x * number, my_list)))
