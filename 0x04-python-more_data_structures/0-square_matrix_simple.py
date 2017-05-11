f780025079c94cdb773c58af9d13ca67607b7af5
KRLoyd <loydkristen@gmail.com>
2017-05-11 20:13:12 +0000

    Remove section to check for empty list

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -9,10 +9,6 @@ def square_matrix_simple(matrix=[]):
     # Check if matrix exists
     if matrix is None:
         return None
-    # # Check if matrix is empty
-    # if len(matrix) == 1 and len(matrix[0]) == 0:
-    #     print()
-    #     return
 
     # Create new_matrix
     new_matrix = []
