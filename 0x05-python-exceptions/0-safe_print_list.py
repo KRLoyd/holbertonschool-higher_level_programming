97d96ed9d20640c5e5cdb22d3f717e36579f456e
KRLoyd <loydkristen@gmail.com>
2017-05-12 18:40:46 +0000

    Create 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/pyhon3
+def safe_print_list(my_list=[], x=0):
+    """ Prints specified elements of a list.
+
+    Args: 
+    my_list -- list to print
+    x -- number of elements to print
+
+    Return: number of elements printed
+    """
+    # Try to print the list
+    try:
+        printed = 0
+        for i, element in enumerate(my_list):
+            if i < x:
+                print("{:d}".format(element), end="")
+                printed += 1
+                i += 1
+    # Except: any error encountered
+    except:
+        pass
+    # Always print new line and return printed
+    finally:
+        print()
+        return printed
