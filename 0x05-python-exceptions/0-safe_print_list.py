0778c6460dc4439389b1a1d4c12d57f29ee5e8bb
KRLoyd <loydkristen@gmail.com>
2017-05-12 18:45:13 +0000

    Removed finally clause. Add print stmt and return in try and except.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -16,10 +16,9 @@ def safe_print_list(my_list=[], x=0):
                 print("{}".format(element), end="")
                 printed += 1
                 i += 1
+        print()
+        return printed
     # Except: any error encountered
     except:
-        pass
-    # Always print new line and return printed
-    finally:
         print()
         return printed
