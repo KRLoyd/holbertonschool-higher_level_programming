e7f9864c1db1aff986e4dedc6093242659956539
KRLoyd <loydkristen@gmail.com>
2017-05-12 20:00:18 +0000

    Create 2-print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """ Prints the first x elements of a list, only integers
+
+    Args:
+    my_list -- list to print
+    x -- number of elements to print
+
+    Return: number of integers printed
+    """
+    # Start print counter at 0
+    print_count = 0
+    # For values up to x
+    for i in range(0, x):
+        # Try to print the element
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            print_count += 1
+        # Exception for non integers
+        except:
+            continue
+    print()
+    return print_count
