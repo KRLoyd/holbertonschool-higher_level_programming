1fdf564128ebd27315a5377d2859fbbe69b32b00
KRLoyd <loydkristen@gmail.com>
2017-05-12 18:56:07 +0000

    remove i += 1 in if stmt

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,7 +15,6 @@ def safe_print_list(my_list=[], x=0):
             if i < x:
                 print("{}".format(element), end="")
                 printed += 1
-                i += 1
         print()
         return printed
     # Except: any error encountered
