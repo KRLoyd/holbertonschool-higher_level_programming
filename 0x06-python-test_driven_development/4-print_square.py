1af288b107b5b21da87c6ab429e9fc8c4154081c
KRLoyd <loydkristen@gmail.com>
2017-05-16 20:57:02 +0000

    Correct PEP8 - Whitespace

--- 0x06-python-test_driven_development/4-print_square.py
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -14,7 +14,7 @@ def print_square(size):
             raise TypeError("size must be an integer")
         else:  # round float and change to int
             size = int(round(size))
-    
+
     # Check if size < 0
     if size < 0:
         raise ValueError("size must be >= 0")
