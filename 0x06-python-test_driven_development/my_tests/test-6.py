51296a096ff7064ce9351eaad844a842398881cd
KRLoyd <loydkristen@gmail.com>
2017-05-17 02:31:29 +0000

    A main file to run max_integer

---
+++ 0x06-python-test_driven_development/my_tests/test-6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+max_integer = __import__('6-max_integer').max_integer
+
+
+# print(max_integer([1, 2, 3, 4]))
+# print(max_integer([1, 3, 4, 2]))
+# print(max_integer([a, 2, 3, 4]))
+# print(max_integer([None]))
+# print(max_integer())
+# print(max_integer([1, 3, 4, 2]))
+# print(max_integer([1, 2, 3, 4]))
+# print(max_integer([1, 3, 4, 2]))
+
+print(max_integer([]))
