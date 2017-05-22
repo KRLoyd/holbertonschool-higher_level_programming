6aa052fee46c84eeea00a4c9dce5e9ec355fb720
KRLoyd <loydkristen@gmail.com>
2017-05-22 23:26:13 +0000

    Add file to check 6-square.py

---
+++ 0x07-python-classes/my_tests/test-6.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+Square = __import__('6-square').Square
+
+print("Square(3)")
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+print("Square(3, (1, 1))")
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+print("Square(3, (3, 0))")
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
+
+print("Square(3, (3, 0, 8))")
+try:
+    my_square_3 = Square(3)
+    my_square_3.position = (3, 0, 8)
+    my_square_3.my_print()
+except Exception as e:
+    print(e)
+
+print("--")
+print("Square(3, (5, 6))")
+my_square_3 = Square(3, (5, 6))
+my_square_3.my_print()
+
+print("--")
+print("Square(3, (1, 0))")
+my_square_3 = Square(3, (1, 0))
+my_square_3.my_print()
+
+print("--")
