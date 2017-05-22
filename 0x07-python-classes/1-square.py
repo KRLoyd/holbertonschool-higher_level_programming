2547e4a99b32eae09fe7628da1260968493c5935
KRLoyd <loydkristen@gmail.com>
2017-05-22 20:38:43 +0000

    Create 1-square.py

---
+++ 0x07-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module Square."""
+
+
+class Square:
+    """ Square class
+
+    Field: size
+    """
+    def __init__(self, size=""):
+        """ __init__ method
+
+        instantiation of size
+        """
+        self.__size = size
