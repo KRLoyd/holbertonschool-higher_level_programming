b47229b86ba4cc7e896463a3a9e175e383797b3d
KRLoyd <loydkristen@gmail.com>
2017-05-31 00:13:48 +0000

    Create 4-inherits_from.py and main

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def inherits_from(obj, a_class):
+    """function inherits_from
+
+    Checks if object is an instance of a class that inherited specific class
+
+    Args:
+    - obj: object to evaluate
+    - a_class: class to compare to
+
+    Returns:
+    - True: object is an instance of a class that inherited specified class
+    - False: object is an instance of a class that inherited specified class
+    """
+    if issubclass(type(obj), a_class) is True:
+        if type(obj) is not a_class:
+            return True
+    else:
+        return False
---
+++ 0x0A-python-inheritance/my_mains/main-4.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+inherits_from = __import__('4-inherits_from').inherits_from
+
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
+print()
+
+a = 1
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
+print()
+
+a = "hey you!"
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
+print()
+
+a = 7.9
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
+print()
+
