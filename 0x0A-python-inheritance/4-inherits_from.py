fbcf76813410cbd51352632471b6aa1f0ef263cf
KRLoyd <loydkristen@gmail.com>
2017-05-31 02:31:29 +0000

    Move check for type to the if stmnt instead of below

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,8 +14,7 @@ def inherits_from(obj, a_class):
     - True: object is an instance of a class that inherited specified class
     - False: object is an instance of a class that inherited specified class
     """
-    if issubclass(type(obj), a_class) is True:
-        if type(obj) is not a_class:
-            return True
+    if issubclass(type(obj), a_class) and not type(obj) == a_class:
+        return True
     else:
         return False
--- 0x0A-python-inheritance/my_mains/main-4.py
+++ 0x0A-python-inheritance/my_mains/main-4.py
@@ -2,6 +2,7 @@
 inherits_from = __import__('4-inherits_from').inherits_from
 
 a = True
+print("a = True")
 if inherits_from(a, int):
     print("{} inherited from class {}".format(a, int.__name__))
 if inherits_from(a, bool):
@@ -11,6 +12,7 @@ if inherits_from(a, object):
 print()
 
 a = 1
+print("a = 1")
 if inherits_from(a, int):
     print("{} inherited from class {}".format(a, int.__name__))
 if inherits_from(a, bool):
@@ -20,6 +22,7 @@ if inherits_from(a, object):
 print()
 
 a = "hey you!"
+print("a = \"hey you!\"")
 if inherits_from(a, int):
     print("{} inherited from class {}".format(a, int.__name__))
 if inherits_from(a, bool):
@@ -29,6 +32,7 @@ if inherits_from(a, object):
 print()
 
 a = 7.9
+print("a = 7.9")
 if inherits_from(a, int):
     print("{} inherited from class {}".format(a, int.__name__))
 if inherits_from(a, bool):
@@ -37,3 +41,10 @@ if inherits_from(a, object):
     print("{} inherited from class {}".format(a, object.__name__))
 print()
 
+a = 1
+print("a = 1")
+print(inherits_from(a, int))
+
+a = [1, 2, 3]
+print("a = [1, 2, 3]")
+print(inherits_from(a, list))
