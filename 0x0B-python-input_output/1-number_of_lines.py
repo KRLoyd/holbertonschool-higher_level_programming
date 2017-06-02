b8e785074fef0154be3f0fb286dc9b27ccd8e81f
KRLoyd <loydkristen@gmail.com>
2017-06-02 20:19:30 +0000

    Remove ; - pep8

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -3,7 +3,7 @@ def number_of_lines(filename=""):
     """method number_of_lines
     Returns the number of lines in a text file.
     """
-    lines = 0;
+    lines = 0
     with open(filename, mode='r', encoding='utf-8') as f:
         for line in f:
             lines += 1
