837a96444052e2c47a452ebc218861c4d82d70de
KRLoyd <loydkristen@gmail.com>
2017-06-02 20:03:19 +0000

    Create files for task 0. 0-read_file.py - function main-0.py - main file my_file_0.txt - text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """method read_file
+    Reads a text file and prints it to stdout.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        content = f.read()
+        print("{:s}".format(content))
