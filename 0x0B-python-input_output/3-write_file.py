c583da23c92e55cb506170c7b80126e9c7ba7d89
KRLoyd <loydkristen@gmail.com>
2017-06-02 21:19:49 +0000

    Completed task 3: 3-write_file.py main-3.py my_first_text_file.txt - file created by running main.3.py

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def write_file(filename="", text=""):
+    """method write_file
+    Writes text to file. 
+    Returns the number of characters written.
+    """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        total_writ = f.write(text)
+        return total_writ
