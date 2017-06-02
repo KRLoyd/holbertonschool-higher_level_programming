7c6810d23c5bb1d656f1ebd469e6c8eb5363a3a5
KRLoyd <loydkristen@gmail.com>
2017-06-02 21:22:07 +0000

    Correct style for pep8

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def write_file(filename="", text=""):
     """method write_file
-    Writes text to file. 
+    Writes text to file.
     Returns the number of characters written.
     """
     with open(filename, mode='w', encoding='utf-8') as f:
