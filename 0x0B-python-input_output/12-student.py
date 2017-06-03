a73223d02a75046bc536a5a510dc2aef99b60f7b
KRLoyd <loydkristen@gmail.com>
2017-06-03 08:19:50 +0000

    Complete task 12: 12-student.py main-12.py - to use 12-student.py

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""module to define class Student"""
+
+
+class Student():
+    """class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """method __init__
+
+        Instantiates the following public instance attributes:
+        - first_name
+        - last_name
+        - age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """method to_json
+
+        Returns a dictionary representation of Student instance.
+        """
+        new_dict = {}
+        if attrs is not None and type(attrs) is list:
+            for i in attrs:
+                if type(i) is str:
+                    if hasattr(self, i) is True:
+                        value = getattr(self, i)
+                        new_dict[i] = value
+            return new_dict
+        return self.__dict__
