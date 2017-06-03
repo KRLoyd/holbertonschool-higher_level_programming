0afabda62cf26a6affcb06c25f4f1a9c8009728c
KRLoyd <loydkristen@gmail.com>
2017-06-03 05:47:40 +0000

    Remove print statements

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -8,13 +8,10 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 try:
     the_list = load_from_json_file("add_item.json")
-    print("the list: {}".format(the_list))
 except:
     the_list = []
-    print("the list: {}".format(the_list))
 
 for i in range(len(sys.argv)):
     if i > 0:
         the_list.append(sys.argv[i])
-print("appended list: {}".format(the_list))
 save_to_json_file(the_list, "add_item.json")
