5b2f78fbfeb22467bc7cfa71602b71d9b4aced8a
KRLoyd <loydkristen@gmail.com>
2017-06-03 05:31:51 +0000

    correct style for pep8

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import json
+
+
 def save_to_json_file(my_obj, filename):
     """method save_to_json_file
     Writes an object to a text file using JSON representation.
     """
     with open(filename, mode='w', encoding='utf-8') as f:
-        json.dump(my_obj, f)
+        return json.dump(my_obj, f)
