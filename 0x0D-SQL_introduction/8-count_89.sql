6bbbe693b217932b43cc6177da739943d1f9abcf
KRLoyd <loydkristen@gmail.com>
2017-07-13 19:29:28 +0000

    Remove id from SELECT and remove GROUP BY

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,6 +1,5 @@
 -- Script that displays the number of records with id = 89 in table first_table
 -- Use SELECT and COUNT
-SELECT id, COUNT(*) 
+SELECT COUNT(*) 
 FROM first_table 
 WHERE id = 89 
-GROUP BY id;
