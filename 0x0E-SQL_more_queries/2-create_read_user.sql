93b3c039f9658961d088e7d4c3e59958596bbecb
KRLoyd <loydkristen@gmail.com>
2017-07-14 03:14:48 +0000

    Remove line to set permissions for *.* for user_0d_2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 -- Set password for the user
 SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
--- Grant all permissions for *.* user_0d_2
-GRANT ALL ON *.* TO 'user_0d_2'@'localhost';
 -- Grant SELECT permission for database hbtn_0d_2 for user_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
