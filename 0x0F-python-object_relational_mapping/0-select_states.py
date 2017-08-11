b88941acb7128c6986692d27833449ae2b4eca47
KRLoyd <loydkristen@gmail.com>
2017-08-11 01:34:16 +0000

    Change ''' comments to double quotes instead

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,15 +7,15 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    '''Get data from database'''
+    """Get data from database"""
     database = MySQLdb.connect(host='localhost', port=3306,
                                user=argv[1], passwd=argv[2], db=argv[3])
     cursor = database.cursor()
     cursor.execute("SELECT states.id, states.name FROM states ORDER BY id ASC")
     result = cursor.fetchall()
-    '''Print results'''
+    """Print results"""
     for row in result:
         print (row)
-    '''Close cursor and connection to database'''
+    """Close cursor and connection to database"""
     cursor.close()
     database.close()
