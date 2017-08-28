30877bcb040555c402f242ca735fc1f19ffd8111
KRLoyd <loydkristen@gmail.com>
2017-08-28 23:27:54 +0000

    Task 5
    - Python sript that takes in a URL
       - sends a request to the URL
       - displays the value of variable X-Response-Id in response header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that fetches URL passed to script.
+Displays the value of variable X-Request-Id.
+Uses requests module.
+"""
+if __name__ == '__main__':
+    # import necessary module
+    import sys
+    import requests
+
+    # Set variables to passed parameter
+    url_val = sys.argv[1]
+
+    # request the head of  URL
+    req = requests.get(url_val)
+    # Get the variable X-Request-Id from header
+    req_var = req.headers['x-request-id']
+
+    # Print it
+    print(req_var)
