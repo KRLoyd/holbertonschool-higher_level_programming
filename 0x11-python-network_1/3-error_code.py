8fcf7d5a28c8ca29b1d84a83f5a6499f50a4782f
KRLoyd <loydkristen@gmail.com>
2017-08-28 22:52:09 +0000

    Task 3
    - Python script to take in a URL
        - sends a request to the URL
        - displays the body of response decoded in utf-8
        - HTTPError exception is handled

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script to take in a URL, sends a request to the URL,
+and displays the body of the response decpded in utf-8.
+Errors are handled and printed.
+"""
+if __name__ == "__main__":
+    # Import necessary modules
+    import sys
+    import urllib.request
+    import urllib.error
+
+    # Set variables per passed parameters
+    url = sys.argv[1]
+
+    # Make request
+    req = urllib.request.Request(url)
+    try:
+        response = urllib.request.urlopen(req)
+    except urllib.error.HTTPError as e:
+        print("Error code: ", e.code)
+    else:
+        decoded_content = response.read().decode('utf-8')
+        print(decoded_content)
