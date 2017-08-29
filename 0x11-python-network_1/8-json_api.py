55cd11272ab2a1f7d73c0d92adb682123125f387
KRLoyd <loydkristen@gmail.com>
2017-08-29 01:01:56 +0000

    Task 8
    Python script to take a letter and send via POST request to http://0.0.0.0:5000/serach_user

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Python script to take in a letter and send a POST request
+to http://0.0.0.0:5000/search_user with a letter as parameter.
+If no user found, displays "No result"
+"""
+if __name__ == "__main__":
+    # Import modules
+    from sys import argv
+    import requests
+
+    # Check passed parameter
+    if len(argv) < 2 or not argv[1].isalpha():
+        print("No Result")
+    else:
+        # Set value to the q variable and passed param
+        value = {'q': argv[1]}
+        # Make the request
+        req = requests.post('http://0.0.0.0:5000/search_user', data=value)
+        # Get JSON of request
+        try:
+            id_val = req.json().get('id')
+            name_val = req.json().get('name')
+            print('[{}] {}'.format(id_val, name_val))
+        except:
+            print("Not a valid JSON")
