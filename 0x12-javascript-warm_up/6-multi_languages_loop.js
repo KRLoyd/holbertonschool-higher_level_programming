8e196e711d432893238042a7f8cec4b43ed24b45
KRLoyd <loydkristen@gmail.com>
2017-09-19 21:41:11 +0000

    Javascript script to print 3 lines using a for loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let stringArray = Array(3);
+stringArray[0] = 'C is fun';
+stringArray[1] = 'Python is cool';
+stringArray[2] = 'Javascript is amazing';
+for (let i = 0; i < 3; i++) {
+  console.log(stringArray[i]);
+}
