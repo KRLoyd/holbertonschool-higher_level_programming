aa5febd8eb27533c02a050580cc798fd3e5425fc
KRLoyd <loydkristen@gmail.com>
2017-09-25 22:57:45 +0000

    Task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Define Class Rectangle
+module.exports = {Rectangle: Rectangle};
+
+function Rectangle (w, h) {
+  if (w > 0 && h > 0) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+Rectangle.prototype.print = function () {
+  for (let i = 0; i < this.height; i++) {
+    console.log('X'.repeat(this.width));
+  }
+};
