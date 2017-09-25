dcfb5f455e822e0a0d1216e251a4ef93c838af5e
KRLoyd <loydkristen@gmail.com>
2017-09-25 23:00:04 +0000

    Correct semistandard styling

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // Define Class Rectangle
-module.exports = {Rectangle: Rectangle}
+module.exports = {Rectangle: Rectangle};
 
 function Rectangle (w, h) {
-    if (w > 0 && h > 0) {
-	this.width = w;
-	this.height = h;
-    }
+  if (w > 0 && h > 0) {
+    this.width = w;
+    this.height = h;
+  }
 }
