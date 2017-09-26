67b63e7448c4909280b13e33201e6b9d1100f6d7
KRLoyd <loydkristen@gmail.com>
2017-09-26 20:43:42 +0000

    Task 1
    Script to write a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Script to write a string to a file
+// If an error occurs while writing, the error object is printed
+const fs = require('fs');
+let fileName = process.argv[2];
+let stringToWrite = process.argv[3];
+
+fs.writeFile(fileName, stringToWrite, 'utf8', function (err) {
+    if (err) {
+	console.log(err);
+    }
+});
