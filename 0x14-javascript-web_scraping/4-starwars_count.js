8b9af4101c71d937c34ead1da07213f3b7993e5d
KRLoyd <loydkristen@gmail.com>
2017-09-26 23:10:57 +0000

    Remove hard cod for URL, URL will be passed

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,21 +2,22 @@
 // Script to print the number of times character "Wedge Antilles" is present
 const request = require('request');
 const characterToFind = 'https://swapi.co/api/people/18/';
-let requestURL = 'http://swapi.co/api/films/';
+let requestURL = process.argv[2];
 
 request(requestURL, function (error, response, body) {
-  let theBody = JSON.parse(body);
-  let movieResults = theBody.results;
-  let count = 0;
-  for (let movieCount = 0;
-       movieCount < theBody.count;
-       movieCount++) {
-    if (movieResults[movieCount].characters.includes(characterToFind)) {
-      count = count + 1;
-    }
-  }
-  console.log(count);
   if (error) {
     console.log(error);
+  } else {
+    let theBody = JSON.parse(body);
+    let movieResults = theBody.results;
+    let count = 0;
+    for (let movieCount = 0;
+	 movieCount < theBody.count;
+         movieCount++) {
+      if (movieResults[movieCount].characters.includes(characterToFind)) {
+        count = count + 1;
+      }
+    }
+    console.log(count);
   }
 });
