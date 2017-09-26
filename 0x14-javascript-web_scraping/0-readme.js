f5aa719d5ddf7cbb06f2897bf2066f8b8dabfe93
KRLoyd <loydkristen@gmail.com>
2017-09-26 20:25:32 +0000

    Remove console.log() stmt

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 // If an error occurs while reading, the error object is printed
 const fs = require('fs');
 let passedFile = process.argv[2];
-console.log('passedFile: ' + passedFile);
+
 fs.readFile(passedFile, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
