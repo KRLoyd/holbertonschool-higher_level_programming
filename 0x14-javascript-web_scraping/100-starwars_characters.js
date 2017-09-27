193989951212c2b6a519f5dfccf94d9684e5e676
KRLoyd <loydkristen@gmail.com>
2017-09-27 20:38:52 +0000

    Task 7
    Script to print all characters of a Star Wars movie.

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// Script to print all characters of a Star Wars movie
+// Movie episode number matches the passed integer
+const request = require('request');
+const episodeNumber = process.argv[2];
+let requestURL = 'http://swapi.co/api/films/' + episodeNumber;
+
+request(requestURL, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let characterList = JSON.parse(body).characters;
+    for (let characterURL in characterList) {
+      request(characterList[characterURL],
+	      function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          let name = JSON.parse(body).name;
+	  console.log(name);
+        }
+      });
+    }
+  }
+});
